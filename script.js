aa64bdfedb279b602fb5f61449fb142d9ec6fcfb
Renusangram <86851631+Renusangram@users.noreply.github.com>
2025-06-10 13:53:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //store weather img
 //sunny, rainy, clear, snow, haze, cloudy, mist, smoke, default
 const img = [
-    "../images/hotcoldpng.png",
-  "../images/rain.png",
-  "../images/clear.png",
-  "../images/snow.png",
-  "../images/haze.png",
-  "../images/cloudy.png",
-"../images/mist.png", "../images/smoke.png", "" 
+    "/images/hotcoldpng.png",
+  "/images/rain.png",
+  "/images/clear.png",
+  "/images/snow.png",
+  "/images/haze.png",
+  "/images/cloudy.png",
+"/images/mist.png", "/images/smoke.png", "" 
 ];
 //get vars
 const bg = document.querySelector(".app");
